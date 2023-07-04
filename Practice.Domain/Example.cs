1cb58c72915a4baa7714385d07384cd7f3b30e62
V0lbD <vova_zl@mail.ru>
2023-07-04 22:28:57 +0300

    Initial commit

---
+++ Practice.Domain/Example.cs
@@ -0,0 +1,81 @@
+﻿namespace Practice.Domain;
+
+public class Example:
+    IEquatable<string>,
+    IEquatable<int>,
+    IEquatable<Example>
+{
+    public int IntValue
+    {
+        get;
+
+        // init;
+    }
+
+    public string StringValue
+    {
+        get;
+    }
+
+    public Example(
+        int @int = 0,
+        string @string = "")
+    {
+        IntValue = @int;
+        StringValue = @string ?? throw new 
+            ArgumentNullException(nameof(@string));
+    }
+    
+    public void Foo()
+    {
+        
+    }
+
+    public override bool Equals(
+        object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+
+        if (obj is Example example)
+        {
+            return IntValue == example.IntValue
+                   && StringValue.Equals(example.StringValue);
+        }
+        
+        return false;
+    }
+    
+    public bool Equals(
+        string @string)
+    {
+        return StringValue.Equals(@string);
+    }
+    
+    public bool Equals(
+        int @int)
+    {
+        return IntValue.Equals((@int));
+    }
+    
+    public bool Equals(
+        Example example)
+    {
+        if (example == null)
+        {
+            return false;
+        }
+
+        
+        
+        return false;
+    }
+}
+
+public class ExampleChild:
+    Example
+{
+    
+}
---
+++ Practice.Launcher.App/OOPeDemo.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice.Launcher.App;
+
+public class OOPeDemo
+{
+
+    private int _myField;
+
+    public OOPeDemo(
+        int myField)
+    {
+        _myField = myField;
+    }
+
+    public void Foo()
+    {
+        Console.WriteLine("лабы тут");
+    }
+
+    public int MyField1
+    {
+        get;
+        set;
+    }
+    
+    public int MyField
+    {
+        get
+        {
+            return _myField;
+        }
+
+        set
+        {
+            _myField = value;
+        }
+    }
+}
