ac2ed5a7e5a38e3580c8373b79b95c02de618773
V0lbD <vova_zl@mail.ru>
2023-07-08 22:36:59 +0300

    Lab2 done

---
+++ Lab2/EnumerableExtensions.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+
+namespace Lab2;
+
+public static class EnumerableExtensions
+{
+    // проверка на уникальность поданных элементов
+    private static void ThrowIfNotDistinct<T>(
+        this IEnumerable<T> values,
+        IEqualityComparer<T> equalityComparer)
+    {
+        if (values.Distinct(equalityComparer).Count() != values.Count())
+        {
+            throw new ArgumentException("ThrowIfNotDistinct: Elements are repeated.", nameof(values));
+        }
+    }
+    
+    /* генерация всех возможных сочетаний из n (кол-во элементов
+    перечисления) по k (с точностью до порядка, элементы могут
+    повторяться) из элементов входного перечисления:
+        Входное перечисление: [1, 2, 3]; k == 2
+        Выходное перечисление: [ [1, 1], [1, 2], [1, 3], [2, 2], [2, 3], [3, 3] ] */
+
+    public static IEnumerable<IEnumerable<T>> GetGenerationCombinationsWithElementRepetition<T>(
+        this IEnumerable<T>? collection, int k, IEqualityComparer<T>? comparer)
+    {
+        Console.WriteLine("-> IEnumerable.GetGenerationCombinationsWithElementRepetition called");
+        
+        if (collection is null)
+        {
+            throw new ArgumentNullException(nameof(collection), "GetGenerationCombinationsWithElementRepetition: collection shouldn't be null.");
+        }
+
+        if (comparer is null)
+        {
+            throw new ArgumentNullException(nameof(collection), "GetGenerationCombinationsWithElementRepetition: comparer shouldn't be null.");
+
+        }
+        
+        ThrowIfNotDistinct(collection, comparer);
+        
+        if (k > collection.Count())
+        {
+            throw new ArgumentException("GetGenerationCombinationsWithElementRepetition: Parameter k must not be less than the length of the collection.", nameof(collection));
+        }
+
+        return collection.GenerationCombinationsWithElementRepetition(k);
+    }
+
+    public static IEnumerable<IEnumerable<T>> GenerationCombinationsWithElementRepetition<T>(
+        this IEnumerable<T> collection, int k)
+    {
+        if (k <= 0)
+        {
+            yield return new List<T>();
+        }
+        else
+        { 
+            int curr = 0; // для исключения повторений самих сочетаний
+            foreach (var i in collection)
+            {
+                foreach (var j in collection.Skip(curr++).GenerationCombinationsWithElementRepetition<T>(k - 1))
+                {
+                    List<T> tmpCollection = new List<T>();
+                    tmpCollection.Add(i);
+                    tmpCollection.AddRange(j);
+                    yield return tmpCollection;
+                }
+            }
+        }
+    }
+    
+    /* генерация всех возможных подмножеств (без повторений) из
+    элементов входного перечисления:
+        Входное перечисление: [1, 2]
+        Выходное перечисление: [ [], [1], [2], [1, 2] ] */
+
+    public static IEnumerable<IEnumerable<T>> GetGenerationSubset<T>(
+        this IEnumerable<T>? collection, IEqualityComparer<T>? comparer)
+    {
+        Console.WriteLine("-> IEnumerable.GenerationSubSet called");
+        
+        if (collection is null)
+        {
+            throw new ArgumentNullException(nameof(collection), "GetGenerationSubset: collection shouldn't be null.");
+        }
+
+        if (comparer is null)
+        {
+            throw new ArgumentNullException(nameof(comparer), "GetGenerationSubset: comparer shouldn't be null.");
+
+        }
+        
+        ThrowIfNotDistinct(collection, comparer);
+        
+        // degree = 2^countCollection, количество подмножеств в множестве
+        int degree = 1, countCollection = collection.Count();
+        int tmp = countCollection;
+        while (tmp > 0)
+        {
+            degree *= 2;
+            tmp--;
+        }
+
+        var resultList = new List<IEnumerable<T>>();
+        var elementList = new List<T>();
+        for (int i = 0; i < degree; i++) // перебор битовых масок
+        {
+            for (int j = 0; j < countCollection; j++) // перебор битов в маске
+            {
+                if (Convert.ToBoolean(i & (1 << j)))
+                {
+                    elementList.Add(collection.ElementAt(j));
+                }
+            }
+            
+            resultList.Add(new List<T>(elementList));
+            elementList.Clear();
+        }
+        
+        return resultList;
+    }
+
+    /* генерация всех возможных перестановок (без повторений) из
+    элементов входного перечисления:
+        Входное перечисление: [1, 2, 3]
+        Выходное перечисление: [ [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1] ] */
+
+    public static IEnumerable<IEnumerable<T>> GetGenerationPermutationWithoutElementRepetition<T>(
+        this IEnumerable<T>? collection, IEqualityComparer<T>? comparer)
+    {
+        Console.WriteLine("-> IEnumerable.GetGenerationPermutationWithoutElementRepetition called");
+        
+        if (collection is null)
+        {
+            throw new ArgumentNullException(nameof(collection), "GenerationPermutation: collection shouldn't be null.");
+        }
+
+        if (comparer is null)
+        {
+            throw new ArgumentNullException(nameof(comparer), "GenerationPermutation: comparer shouldn't be null.");
+
+        }
+        
+        ThrowIfNotDistinct(collection, comparer);
+        
+        var resultList = new List<T>(collection);
+        
+        return resultList.GenerationPermutationWithoutElementRepetition();
+    }
+
+    public static IEnumerable<IEnumerable<T>> GenerationPermutationWithoutElementRepetition<T>(
+        this IEnumerable<T> collection)
+    {
+        if (collection.Count() == 0) // если все элементы использованы, заканчиваем рекурсию
+        {
+            yield return new List<T>();
+        }
+
+        foreach (var i in collection)
+        {
+            var next  = collection.Where(l => !(l.Equals(i))).ToList();
+            foreach (var perm in GenerationPermutationWithoutElementRepetition(next))
+            {
+                yield return (new List<T>{i}).Concat(perm);
+            }
+        }
+    }
+}
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,3 +1,73 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Lab2;
+
+namespace Lab2
+{
+    class Program
+    {
+        class IntComparer : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return x == y;
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+
+        public static string EnumerableEnumerableToString<T>(IEnumerable<IEnumerable<T>> collections)
+        {
+            string result = "";
+            result += "[";
+
+            foreach (var collection in collections)
+            {
+                string collectionString = "";
+                collectionString += " [ ";
+                collectionString += string.Join(", ", collection);
+                collectionString += " ],";
+                result += collectionString;
+            }
+            
+            result = result[..^1];
+            result += " ]";
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] set1 = { 1, 2, 3};
+                int[] set2 = { 1, 2 };
+                int[] set3 = { 1, 2, 3 };
+                var comparer = new IntComparer();
+
+                var list = new List<int>();
+                
+                // test combinations
+                Console.WriteLine(EnumerableEnumerableToString(
+                    set1.GetGenerationCombinationsWithElementRepetition(2, comparer)));
+                
+                // test subset
+                Console.WriteLine(EnumerableEnumerableToString(
+                    set2.GetGenerationSubset(comparer)));
+                
+                // test permutation
+                Console.WriteLine(EnumerableEnumerableToString(
+                    set3.GetGenerationPermutationWithoutElementRepetition(comparer)));
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }   
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
