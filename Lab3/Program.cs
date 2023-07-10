d06b394c338cfebeca5a1d9f258215baf55ad227
V0lbD <vova_zl@mail.ru>
2023-07-11 02:06:06 +0300

    Реализованы сортировки и добавлена демонстрация функционала методов

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -6,19 +6,65 @@ namespace Lab3
 {
     class Program
     {
+        sealed class IntComparerWithComparer : Comparer<int>
+        {
+            public override int Compare(int x, int y)
+            {
+                return (x - y);
+            }
+        }
+        public static void ToStringArray<T>(T[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+        public static int Comparison(int x, int y)
+        {
+            return (x - y);
+        }
         static void Main(string[] args)
         {
             try
             {
-                var list = new List<int>();
-                list.Add(1);
-                list.Add(6);
-                list.Add(3);
-                list.Add(10);
-                list.Add(-5);
-                list.Sort();
+                int min = -1000;
+                int max = 1000;
+                Random randNumber = new Random();
+                var comparer = new IntComparerWithComparer();
+                int[] set = Enumerable
+                    .Repeat(0, 10)
+                    .Select(i => randNumber.Next(min, max))
+                    .ToArray();
+
+                var insertionSorted = set.Sort(SortExtensions.SortingOrder.Ascending, SortExtensions.SortingAlgorithm.InsertionSort, comparer);
+                var selectionSorted = set.Sort(SortExtensions.SortingOrder.Ascending, SortExtensions.SortingAlgorithm.SelectionSort);
+                var heapSorted = set.Sort(SortExtensions.SortingOrder.Ascending, SortExtensions.SortingAlgorithm.HeapSort, Comparison);
+                var quickSorted = set.Sort(SortExtensions.SortingOrder.Ascending, SortExtensions.SortingAlgorithm.QuickSort);
+                var mergeSorted = set.Sort(SortExtensions.SortingOrder.Ascending, SortExtensions.SortingAlgorithm.MergeSort, comparer);
                 
-                Console.WriteLine(list.ToString());
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine("Original array:");
+                ToStringArray(set);
+                Console.WriteLine("-----------------------------------------------------");
+                
+                Console.WriteLine("Selection sorting:");
+                ToStringArray(selectionSorted);
+                Console.WriteLine("-----------------------------------------------------");
+                
+                Console.WriteLine("Insertion sorting:");
+                ToStringArray(insertionSorted);
+                Console.WriteLine("-----------------------------------------------------");
+
+                Console.WriteLine("Merge sorting");
+                ToStringArray(mergeSorted);
+                Console.WriteLine("-----------------------------------------------------");
+
+                Console.WriteLine("Heap sorting");
+                ToStringArray(heapSorted);
+                Console.WriteLine("-----------------------------------------------------");
+
+                Console.WriteLine("Quick sorting");
+                ToStringArray(quickSorted);
+                Console.WriteLine("-----------------------------------------------------");
+
             }
             catch (ArgumentNullException exception)
             {
--- Lab3/SortExtensions.cs
+++ Lab3/SortExtensions.cs
@@ -18,7 +18,6 @@ public static class SortExtensions
     }
     
     #region перегруженные методы расширения метода List.Sort()
-    
     // четвёртого параметра нет
     public static T[] Sort<T>(
         this T[] collectionToSort, SortingOrder sortingOrder, SortingAlgorithm sortingAlgorithm)
@@ -66,7 +65,6 @@ public static class SortExtensions
     #endregion
 
     #region вспомогательные методы для сортировок
-
     private static void Swap<T>(T[] a, int i, int j)
     {
         (a[i], a[j]) = (a[j], a[i]); // swap two variables without using a temporary variable
@@ -74,61 +72,180 @@ public static class SortExtensions
     
     private static bool CompareInner<T>(T x, T y, SortingOrder sortingOrder, IComparer<T> comparer)
     {
-        return ((sortingOrder == SortingOrder.Ascending) && (comparer.Compare(x, y) < 0));
+        return ((sortingOrder == SortingOrder.Ascending) & (comparer.Compare(x, y) < 0));
     }
-    
     #endregion
 
+    
+    #region алгоритмы сортировки
+    
     #region InsertionSort
-
-    private static T[] InsertionSort<T>(T[] keys, SortingOrder sortingMode, IComparer<T> comparer)
+    private static T[] InsertionSort<T>(T[] keys, SortingOrder sortingOrder, IComparer<T> comparer)
     {
+        T[] toReturn = new T[keys.Length];
+        toReturn[0] = keys[0];
         
-    }
+        for (int i = 0; i < keys.Length - 1; i++)
+        {
+            T t = keys[i + 1];
 
+            int j = i;
+            while (j >= 0 && CompareInner(t, toReturn[j], sortingOrder, comparer))
+            {
+                toReturn[j + 1] = toReturn[j];
+                j--;
+            }
+                
+            toReturn[j + 1] = t;
+        }
+
+        return toReturn;
+    }
     #endregion
 
     #region SelectionSort
-
-    private static T[] SelectionSort<T>(T[] keys, SortingOrder sortingMode, IComparer<T> comparer)
+    private static T[] SelectionSort<T>(T[] keys, SortingOrder sortingOrder, IComparer<T> comparer)
     {
-        
-    }
+        T[] toReturn = (T[])keys.Clone();
 
+        for (int i = 0; i < toReturn.Length - 1; i++)
+        {
+            int min = i;
+            for (int j = i + 1; j < toReturn.Length; j++)
+            {
+                if (CompareInner(toReturn[j], toReturn[min], sortingOrder, comparer))
+                {
+                    min = j;
+                }
+            }
+
+            if (min != i)
+            {
+                Swap(toReturn, i, min);
+            }
+        }
+        return toReturn;
+    }
     #endregion
 
     #region HeapSort
-
-    private static T[] HeapSort<T>(T[] keys, SortingOrder sortingMode, IComparer<T> comparer)
+    private static T[] HeapSort<T>(T[] keys, SortingOrder sortingOrder, IComparer<T> comparer)
     {
+        T[] toReturn = (T[])keys.Clone();
         
-    }
+        int n = toReturn.Length;
+        for (int i = n >> 1; i >= 1; i--)
+        {
+            DownHeap(toReturn, i, n, sortingOrder, comparer);
+        }
 
+        for (int i = n; i > 1; i--)
+        {
+            Swap(toReturn, 0, i - 1);
+            DownHeap(toReturn, 1, i - 1, sortingOrder, comparer);
+        }
+
+        return toReturn;
+    }
+    
+    private static void DownHeap<T>(T[] keys, int i, int n, SortingOrder sortingOrder, IComparer<T> comparer)
+    {
+        T tmp = keys[i - 1];
+        while (i <= n >> 1)
+        {
+            int child = 2 * i;
+            if (child < n && CompareInner(keys[child - 1], keys[child], sortingOrder, comparer))
+            {
+                child++;
+            }
+
+            if (!CompareInner(tmp, keys[child - 1], sortingOrder, comparer))
+                break;
+
+            keys[i - 1] = keys[child - 1];
+            i = child;
+        }
+
+        keys[i - 1] = tmp;
+    }
     #endregion
     
     #region QuickSort
-
     private static T[] QuickSort<T>
-        (T[] keys, SortingOrder sortingMode, IComparer<T> comparer)
+        (T[] keys, SortingOrder sortingOrder, IComparer<T> comparer)
     {
+        {
+            T[] toReturn = (T[])keys.Clone();
         
+            QuickSortingInner(toReturn, 0, toReturn.Length - 1, sortingOrder, comparer);
+        
+            return toReturn;
+        }
     }
-
+    
+    private static void QuickSortingInner<T>
+        (T[] keys, int leftBound, int rightBound, SortingOrder sortingOrder, IComparer<T> comparer)
+    {
+        if (leftBound < rightBound)
+        {
+            int pivot = Partition(keys, leftBound, rightBound, sortingOrder, comparer);
+            QuickSortingInner(keys, leftBound, pivot - 1, sortingOrder, comparer);
+            QuickSortingInner(keys, pivot + 1, rightBound, sortingOrder, comparer);
+        }
+    }
+    
+    private static int Partition<T> // для разделения обобщённого массива данных
+        (T[] keys, int leftBound, int rightBound, SortingOrder sortingOrder, IComparer<T> comparer)
+    {
+        T pivot = keys[rightBound];
+        int i = leftBound - 1;
+        for (int j = leftBound; j < rightBound; j++)
+        {
+            if (CompareInner(keys[j], pivot, sortingOrder, comparer))
+            {
+                ++i;
+                Swap(keys, i, j);
+            }
+        }
+        
+        Swap(keys, i + 1, rightBound);
+        return (i + 1);
+    }
     #endregion
 
     #region MergeSort
 
-    private static T[] MergeSort<T>(T[] keys, SortingOrder sortingMode, IComparer<T> comparer)
+    private static T[] MergeSort<T>(T[] keys, SortingOrder sortingOrder, IComparer<T> comparer)
     {
+        T[] toReturn = (T[])keys.Clone();
+        if (keys.Length == 1) return toReturn;
+        int middle = keys.Length / 2;
         
+        return Merge(MergeSort(toReturn.Take(middle).ToArray(), sortingOrder, comparer), 
+            MergeSort(toReturn.Skip(middle).ToArray(), sortingOrder, comparer), sortingOrder, comparer);
+    }
+    
+    private static T[] Merge<T>(T[] first, T[] second, SortingOrder sortingOrder, IComparer<T> comparer)
+    {
+        int ptr1 = 0, ptr2 = 0;
+        T[] merged = new T[first.Length + second.Length];
+
+        for (int i = 0; i < merged.Length; ++i)
+        {
+            if (ptr1 < first.Length && ptr2 < second.Length)
+            {
+                merged[i] = CompareInner(second[ptr2], first[ptr1], sortingOrder, comparer) ? second[ptr2++] : first[ptr1++];
+            }
+            else
+            {
+                merged[i] = ptr2 < second.Length ? second[ptr2++] : first[ptr1++];
+            }
+        }
+		
+        return merged;
     }
 
     #endregion
-    
-    
-    #region алгоритмы сортировки
-    
-    
-    
+
     #endregion
 }
