ce46031509898e5242a1fc850a1b8b8facd13fc7
V0lbD <vova_zl@mail.ru>
2023-07-11 02:28:27 +0300

    Реализован интерфейс IIntegralCalculation

--- Lab4/IIntegralCalculation.cs
+++ Lab4/IIntegralCalculation.cs
@@ -2,5 +2,24 @@
 
 public interface IIntegralCalculation
 {
+    public string? MethodName
+    {
+        get;
+    }
     
+    public delegate double Integrand(double x); // delegate for integrand
+    public static void CheckBoundsAndEps(ref double lowerBound, ref double upperBound, ref double eps)
+    {
+        if (eps <= 0)
+        {
+            eps = -eps;
+        }
+        
+        if (lowerBound > upperBound)
+        {
+            (lowerBound, upperBound) = (upperBound, lowerBound);
+        }
+    }
+
+    public (double, int) IntegralCalculation(Integrand integrand, double lowerBound, double highBound, double eps);
 }
